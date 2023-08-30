011eaa14aec98642d8303c7b3eba29aaa2b7ef44
kaiihayes <hayeskaitlyn10@gmail.com>
2023-08-30 02:01:59 -0500

    finished homepage mockup

---
+++ HelpfulNeighbor.web/ClientApp/src/Pages/AboutUsPage.tsx
@@ -0,0 +1,8 @@
+
+export default function AboutUsPage () {
+    return(
+        <div>
+            Learn more about the founders of Helpful Neighbor!
+        </div>
+    )
+}
---
+++ HelpfulNeighbor.web/ClientApp/src/Pages/FeedbackPage.tsx
@@ -0,0 +1,8 @@
+
+export default function FeedbackPage () {
+    return(
+        <div>
+            Feedback Page
+        </div>
+    )
+}
