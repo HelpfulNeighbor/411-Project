a7405f877fcc624e127c641adfd31c6c34f5f578
AutumnKennedy <autumnkennedy0714@gmail.com>
2023-09-15 11:20:30 -0500

    changed databases

---
+++ HelpfulNeighbor.web/Interfaces/IShelterRepository.cs
@@ -0,0 +1,9 @@
+﻿using HelpfulNeighbor.web.Models;
+
+namespace HelpfulNeighbor.web.Interfaces
+{
+    public interface IShelterRepository
+    {
+        ICollection<Shelter> GetShelters();
+    }
+}
