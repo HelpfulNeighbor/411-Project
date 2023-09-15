931469ee6d16731cea5df9b74d823e0ab45bdd39
AutumnKennedy <autumnkennedy0714@gmail.com>
2023-09-15 00:54:36 -0500

    created get endpoints on shelter and resource controllers

--- HelpfulNeighbor.web/Repositories/ShelterRepository.cs
+++ HelpfulNeighbor.web/Repositories/ShelterRepository.cs
@@ -1,18 +1,19 @@
 ﻿using HelpfulNeighbor.web.Data;
+using HelpfulNeighbor.web.Interfaces;
 using HelpfulNeighbor.web.Models;
 
 namespace HelpfulNeighbor.web.Repositories
 {
-    public class ShelterRepository
+    public class ShelterRepository : IShelterRepository
     {
         private readonly DataContext _context;
         public ShelterRepository(DataContext context)
         {
             _context = context;
         }
-        public ICollection<Shelter> GetResources()
+        public ICollection<Shelter> GetShelters()
         {
-            return _context.Shelters.OrderBy(s => s.Name).ToList();
+            return _context.Shelters.OrderBy(s => s.ResourceId).ToList();
         }
     }
 }
