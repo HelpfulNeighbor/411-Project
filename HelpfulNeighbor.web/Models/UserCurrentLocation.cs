5e5af4ef2fb6862c096d08c17851581c9c813c0a
AutumnKennedy <autumnkennedy0714@gmail.com>
2023-09-19 09:36:15 -0500

    changed comments

--- HelpfulNeighbor.web/Models/UserCurrentLocation.cs
+++ HelpfulNeighbor.web/Models/UserCurrentLocation.cs
@@ -2,8 +2,8 @@
 {
     public class UserCurrentLocation
     {
-        public int UserId { get; set; } // Foreign key - modify later
-        public int LocationId { get; set; } // Foreign key - modify later
+        public int UserId { get; set; } // Foreign key 
+        public int LocationId { get; set; } // Foreign key 
         public DateTime Timestamp { get; set; }
         public User User { get; set; } // Navigation property to User (one-to-one)
         public Location Location { get; set; } // Navigation property to Location (one-to-one)
