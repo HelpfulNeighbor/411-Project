c152306fb0c75759bf29305cb38b60839d5c3540
AutumnKennedy <autumnkennedy0714@gmail.com>
2023-09-19 08:29:09 -0500

    Updated DataContext

--- HelpfulNeighbor.web/Data/DataContext.cs
+++ HelpfulNeighbor.web/Data/DataContext.cs
@@ -10,25 +10,50 @@ namespace HelpfulNeighbor.web.Data
         {
             
         }
+        public DbSet<HoursOfOperation> HoursOfOperations { get; set; }
+        public DbSet<Location> Locations { get; set; }
         public DbSet<Resource> Resources { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<SavedResource> SavedResources { get; set; }
+        public DbSet<SavedShelter> SavedShelters { get; set; }
         public DbSet<Shelter> Shelters { get; set; }
         public DbSet<User> Users { get; set; }
-        public DbSet<Location> Locations { get; set; }
+        public DbSet<UserCurrentLocation> UserCurrentLocations { get; set; }
         public DbSet<UserRole> UserRoles { get; set; }
-        public DbSet<Role> Roles { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            // Resource
             modelBuilder.Entity<Resource>()
                 .HasKey(r => r.ResourceId);
+
+            //Shelter
             modelBuilder.Entity<Shelter>()
                 .HasKey(r => r.ResourceId);
+
+            //User
             modelBuilder.Entity<User>()
                 .HasKey(u => u.UserId);
+
+            //Location
             modelBuilder.Entity<Location>()
                 .HasKey(l => l.LocationId);
+
+            //UserRole
             modelBuilder.Entity<UserRole>()
-                .HasKey(ur => ur.UserRoleId);
+            .HasKey(ur => new { ur.UserId, ur.RoleId });
+
+            modelBuilder.Entity<UserRole>()
+                .HasOne(ur => ur.User)
+                .WithMany(u => u.UserRoles)
+                .HasForeignKey(ur => ur.UserId);
+
+            modelBuilder.Entity<UserRole>()
+                .HasOne(ur => ur.Role)
+                .WithMany()
+                .HasForeignKey(ur => ur.RoleId);
+
+            //Role
             modelBuilder.Entity<Role>()
                 .HasKey(r => r.RoleId);
         }
