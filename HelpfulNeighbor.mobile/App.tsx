4dd45288a1395d4ef2a9442796ea59c4cadda48d
PaytonLaskie <payton.laskie@gmail.com>
2023-10-06 16:35:56 -0500

    Setting up the screens for mobile

--- HelpfulNeighbor.mobile/App.tsx
+++ HelpfulNeighbor.mobile/App.tsx
@@ -1,13 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './screens/HomeScreen/HomeScreen';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+       <View style={styles.container}>
+        <NavigationContainer>
+        <Button
+  onPress={() => {
+    console.log('You tapped the button!');
+  }}
+  title="Press Me"
+/>
+        </NavigationContainer>
+       </View>
+    );
 }
 
 const styles = StyleSheet.create({
---
+++ HelpfulNeighbor.mobile/screens/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Button } from "@react-native-material/core";
+
+const App = () => (
+    <Button title="Click Me"/>
+
+);
+
+export default App;
