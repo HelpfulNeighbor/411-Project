7779607c2c0dc5dcb40bc8feca4f98e3258b4588
AutumnKennedy <autumnkennedy0714@gmail.com>
2023-10-24 22:07:47 -0500

    Setup automapper for resources

---
+++ HelpfulNeighbor.web/Helper/MappingProfiles.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using HelpfulNeighbor.web.Features.Dto;
+using HelpfulNeighbor.web.Features.Models;
+
+namespace HelpfulNeighbor.web.Helper
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+                CreateMap<Resource,ResourceDto>();
+        }
+    }
+}
