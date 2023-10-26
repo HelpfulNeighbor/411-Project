b20375fe5bfa2c7c3d6007015d1a6708c163969b
kaiihayes <hayeskaitlyn10@gmail.com>
2023-10-26 13:47:20 -0500

    added "s" to http

--- HelpfulNeighbor.web/ClientApp/src/Api/config.tsx
+++ HelpfulNeighbor.web/ClientApp/src/Api/config.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: "https://localhost:44357"
-  baseURL: "http://helpfulneighborweb20230917140513.azurewebsites.net/"
+  baseURL: "https://helpfulneighborweb20230917140513.azurewebsites.net/"
 });
 
 export default api;
