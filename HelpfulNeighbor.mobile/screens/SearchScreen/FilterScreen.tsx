c25179555cbe2fc18f7e364dc062a214e4cff78e
AutumnKennedy <autumnkennedy0714@gmail.com>
2023-11-26 19:52:10 -0600

    temp changes

--- HelpfulNeighbor.mobile/screens/SearchScreen/FilterScreen.tsx
+++ HelpfulNeighbor.mobile/screens/SearchScreen/FilterScreen.tsx
@@ -1,40 +1,30 @@
+// FilterScreen.tsx
+
 import * as React from 'react';
 import Style from '../../screens/SearchScreen/Style';
-import { RootStackNavProps } from "../../data/types/NavigationTypes";
-import { Button, Text } from "react-native-paper";
+import { Text } from "react-native-paper";
 import { SafeAreaView, ScrollView } from 'react-native';
 import ResourceTypeFilter from '../../components/Search/Map/ResourceTypeFilter';
 import ParishFilter from '../../components/Search/Map/ParishFilter';
+import { FilterScreenProps } from '../../data/types/NavigationTypes';
 
+const FilterScreen: React.FC<FilterScreenProps> = ({ navigation, onFilterChange, checkedResourceTypes, checkedParishList }) => {
+  return (
+    <SafeAreaView style={Style.container}>
+      <ScrollView>
+        <Text style={Style.title}>Filter Your Search</Text>
+        <ResourceTypeFilter 
+          onFilterChange={onFilterChange} 
+          checkedResourceTypes={checkedResourceTypes} 
+        />
+        <Text>{'\n'}</Text>
+        <ParishFilter 
+          onFilterChange={onFilterChange} 
+          checkedParishList={checkedParishList} 
+        />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
-interface FilterScreenProps extends RootStackNavProps<'Filter'> {
-  onFilterChange: (filterName: string, filterValue: boolean, filterCategory: string) => void;
-  checkedResourceTypes: string[];
-  checkedParishList: string[];
-}
-
-  const FilterScreen: React.FC<FilterScreenProps> = ({ navigation, onFilterChange, checkedResourceTypes, checkedParishList }) => {
-    const handleGoBack = () => {
-      navigation.goBack();
-    };
-  
-    return (
-      <SafeAreaView style={Style.container}>
-        <ScrollView>
-          <Text style={Style.title}>Filter Your Search</Text>
-          <ResourceTypeFilter 
-            onFilterChange={onFilterChange} 
-            checkedResourceTypes={checkedResourceTypes} 
-          />
-          <Text>{'\n'}</Text>
-          <ParishFilter 
-            onFilterChange={onFilterChange} 
-            checkedParishList={checkedParishList} 
-          />
-          <Button onPress={handleGoBack}>Go Back</Button>
-        </ScrollView>
-      </SafeAreaView>
-    );
-  };
-  
-  export default FilterScreen;
+export default FilterScreen;
