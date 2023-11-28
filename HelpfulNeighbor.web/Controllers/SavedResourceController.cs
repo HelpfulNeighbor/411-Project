e585f1612b5d8b44eb8534e5306d91cbb55586b9
AutumnKennedy <autumnkennedy0714@gmail.com>
2023-11-28 07:50:37 -0600

    Created Get, Post, and Delete endpoints

---
+++ HelpfulNeighbor.web/Controllers/SavedResourceController.cs
@@ -0,0 +1,96 @@
+﻿using AutoMapper;
+using HelpfulNeighbor.web.Features.Dto;
+using HelpfulNeighbor.web.Features.Interfaces;
+using HelpfulNeighbor.web.Features.Models;
+using HelpfulNeighbor.web.Features.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+
+namespace HelpfulNeighbor.web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SavedResourceController : Controller
+    {
+        private readonly ISavedResourceRepository _savedResourceRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly IResourceRepository _resourceRepository;
+        private readonly IMapper _mapper;
+
+        public SavedResourceController(ISavedResourceRepository savedResourceRepository, IUserRepository userRepository, IResourceRepository resourceRepository, IMapper mapper)
+        {
+            _savedResourceRepository = savedResourceRepository;
+            _userRepository = userRepository;
+            _resourceRepository = resourceRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet("{userId}")]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<SavedResource>))]
+        public IActionResult GetSavedResources(int userId)
+        {
+            if (!_userRepository.UserExist(userId))
+                return NotFound();
+
+            var savedResources = _mapper.Map<List<SavedResourceDto>>(_savedResourceRepository.GetSavedResources(userId));
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok(savedResources);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        public IActionResult CreateSavedResource([FromBody] SavedResourceDto savedResourceDto)
+        {
+            if (savedResourceDto == null)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (!_userRepository.UserExist(savedResourceDto.UserId) || !_resourceRepository.ResourceExist(savedResourceDto.ResourceId))
+            {
+                return BadRequest("Invalid user id or resource id");
+            }
+
+            var savedResource = _mapper.Map<SavedResource>(savedResourceDto);
+
+            if (_savedResourceRepository.SavedResourceExists(savedResource.UserId, savedResource.ResourceId))
+            {
+                return BadRequest("Resource already saved by the user");
+            }
+
+            _savedResourceRepository.CreateSavedResource(savedResource);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{userId}/{resourceId}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        public IActionResult DeleteSavedResource(int userId, int resourceId)
+        {
+            if (!_userRepository.UserExist(userId) || !_resourceRepository.ResourceExist(resourceId))
+            {
+                return BadRequest("Invalid user id or resource id");
+            }
+
+            var savedResource = _savedResourceRepository.GetSavedResource(userId, resourceId);
+            if (savedResource == null)
+            {
+                return BadRequest("Saved resource not found");
+            }
+
+            _savedResourceRepository.DeleteSavedResource(savedResource);
+
+            return NoContent();
+        }
+
+
+
+    }
+}
--- HelpfulNeighbor.web/Features/Interfaces/ISavedResourceRepository.cs
+++ HelpfulNeighbor.web/Features/Interfaces/ISavedResourceRepository.cs
@@ -4,6 +4,16 @@ namespace HelpfulNeighbor.web.Features.Interfaces
 {
     public interface ISavedResourceRepository
     {
-        public ICollection<SavedResource> GetSavedResources();
+        public ICollection<SavedResource> GetSavedResources(int userId);
+
+        bool CreateSavedResource(SavedResource savedResource);
+
+        bool SavedResourceExists(int userId, int resourceId);
+
+        SavedResource GetSavedResource(int userId, int resourceId);
+
+        bool DeleteSavedResource(SavedResource savedResource);
+
+        bool Save();
     }
 }
