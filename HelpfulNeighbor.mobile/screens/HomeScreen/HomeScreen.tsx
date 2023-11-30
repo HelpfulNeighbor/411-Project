9ca490d4788bd3614026f8c82c6f9a1777bd02a2
kaiihayes <hayeskaitlyn10@gmail.com>
2023-11-29 18:11:18 -0600

    fixed missing stuff for mobile

--- HelpfulNeighbor.mobile/screens/HomeScreen/HomeScreen.tsx
+++ HelpfulNeighbor.mobile/screens/HomeScreen/HomeScreen.tsx
@@ -1,19 +1,16 @@
 import React from "react";
-import { SafeAreaView, ScrollView,Text } from "react-native";
+import { SafeAreaView, ScrollView, Text } from "react-native";
 import Style from "./Style";
 import InfoCard from "../../components/Home/InfoCard";
 const HomeScreen = () => {
-
-
-    return (
-        <SafeAreaView style={Style.container}>
-            <ScrollView>
-                
-                <Text style ={Style.title}>Helpful Neighbor</Text>
-                < InfoCard />
-            </ScrollView>
-        </SafeAreaView>
-    )
-}
+  return (
+    <SafeAreaView style={Style.container}>
+      <ScrollView>
+        <Text style={Style.title}>Helpful Neighbor</Text>
+        <InfoCard />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 export default HomeScreen;
