8bb82f49abd8a6e5d4d87d13b01f842e7426b89f
PaytonLaskie <payton.laskie@gmail.com>
2023-11-29 18:18:15 -0600

    fixed merge conflicts

--- HelpfulNeighbor.mobile/components/Search/SavedResourceCard.tsx
+++ HelpfulNeighbor.mobile/components/Search/SavedResourceCard.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-    View
+    View,
+    TouchableWithoutFeedback
 } from 'react-native';
 import { 
     Text, 
@@ -8,6 +9,7 @@ import {
     Card
 } from 'react-native-paper';
 import { FontAwesome5 as FaIcon } from '@expo/vector-icons';
+
 
 interface HoursOfOperation {
     DayOfWeek: string;
@@ -27,39 +29,58 @@ interface SavedResourceCardProps {
     Distance: number;
 }
 
+
 const SavedResourceCard: React.FC<SavedResourceCardProps> = (props) => {
-    return <>   
+        const [expanded, setExpanded] = useState(false);
+        const toggleDropdown = () => {
+            setExpanded(!expanded);
+        };
+
+    return ( 
+        <>   
         <Card style={{ margin: 15 }}>
             <Card.Content>
-                <Text>{props.Name} Resource Name Here</Text>
+                <Text>{props.Name} Example Homeless Shelter</Text>
                 <Text>{props.ResourceType || ''}</Text>
                 <Text>
                     <FaIcon name="running" color="#7a5cce" /> {props.Distance.toFixed(2)} mi.
                 </Text>
                 <Text>
-                    <FaIcon name="location-dot" color="#7a5cce" /> {props.Address || ''}
+                    <FaIcon name="map-marker-alt"color="#7a5cce" /> {props.Address || ''}
+                    123 Example Street {'\n'}
+                    Baton Rouge, LA
                 </Text>
                 <Text>
                     <FaIcon name="globe-americas" color="#7a5cce" /> {props.Website || ''}
+                    www.websiteurl.com
                 </Text>
                 <Text>
                     <FaIcon name="phone-alt" color="#7a5cce" /> {props.PhoneNumber || ''}
+                    (123)-456-7890
                 </Text>
-            </Card.Content>
-            {props.hoursOfOperation && props.hoursOfOperation.length > 0 ? (
-                <Card.Content>
+            <TouchableWithoutFeedback onPress={toggleDropdown}>
+                <View>
+                    <Text>Hours of Operation:</Text>
+                </View>
+            </TouchableWithoutFeedback>
+            
+            {expanded && props.hoursOfOperation && props.hoursOfOperation.length > 0 ? (
+                <View>
                     {props.hoursOfOperation.map((hours, index) => (
                         <View key={index}>
                             <Text>{hours.DayOfWeek || 'N/A'}</Text>
                             <Text>Open: {hours.OpenTime || 'N/A'}</Text>
                             <Text>Close: {hours.CloseTime || 'N/A'}</Text>
                         </View>
-                    ))}
+                        ))}
+                        </View>
+                    ) : (
+                        <Text>N/A</Text>
+                    )}
                 </Card.Content>
-            ) : (
-                <Text>N/A</Text>
-            )}
-        </Card></>;
-}
+            </Card>
+        </>
+    );
+};
 
 export default SavedResourceCard;
--- HelpfulNeighbor.mobile/screens/ProfileScreen/ProfileScreen.tsx
+++ HelpfulNeighbor.mobile/screens/ProfileScreen/ProfileScreen.tsx
@@ -2,9 +2,13 @@ import * as React from 'react';
 import { SafeAreaView, ScrollView, Text } from "react-native";
 import Style from "./Style";
 import SavedResourceCard from '../../components/Search/SavedResourceCard';
+import { AuthData } from '../../data/types/UserTypes';
 
 const ProfileScreen = () => {
 
+const ProfileScreen: React.FC<ProfileScreenProps> = () => {
+  const [userData, setUserData] = useState<AuthData | null>(null);
+  const [visible, setVisible] = useState<boolean>(false);
 
     return (
         <SafeAreaView style={Style.container}>
